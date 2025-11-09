4250429d0e29eeab4fc9c369447330ba8e7b6312
prasantgitgub911 <prasantkumarpatra@gmail.com>
2025-11-09 09:27:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region to launch the instance"
-  default     = "ap-south-1"
+  default     = "eu-north-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
-  default     = "ami-01760eea5c574eb8" # Example This is our free ami in ap-south-1
+  default     = "ami-07fb0a5bf9ae299a4" # Example This is our free ami in ap-south-1
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.micro"
+  default     = "t2.small"
 }
